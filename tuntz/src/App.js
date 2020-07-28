404ef1fdbc18d1db42fa262196f53bf68ec65fc2
felipecsaraiva <felipecsaraiva@gmail.com>
2020-07-28 15:55:37 -0300

    Projeto TUNTZ

---
+++ tuntz/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from './assets/img/tuntz.png';
+
+function App() {
+  return (
+    <div className="LogoDiv">
+      <img className="Logo" src={Logo} alt="Tuntz" />
+    </div>
+  );
+}
+
+export default App;
